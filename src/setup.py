537bd57f2a31abaff179fd3b906a5da9a6128617
nick <nick@AmayaL>
2010-03-14 13:07:55 -0600

    Finished Setup.py file

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='CherrySlice',
+      version=0.1,
+      description='CherryPy Extention Framework',
+      author='Nick Verbeck',
+      author_email='nerdynick@gmail.com',
+      packages=['cherryslice'],
+     )
