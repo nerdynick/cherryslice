a3f4a6407fcbf58726d13e5e5cc7ef2e55cc7518
Nick Verbeck <nerdynick@gmail.com>
2010-08-27 23:40:42 -0600

    Added Memcached Caching Layer

---
+++ src/cherryslice/lib/memCaching.py
@@ -0,0 +1,24 @@
+'''
+Created on Aug 27, 2010
+
+@author: nick
+'''
+import memcache
+import cherrypy
+
+def getInstance():
+    return MemcachedCache()
+
+class MemcachedCache(object):
+    def __init__(self):
+        self.mem = memcache.Client(str(cherrypy.request.app.config["memcached"]['servers']).split(','))
+        
+    def loads(self, key):
+        return self.mem.get(key)
+    get = loads
+        
+    def dumps(self, key, value, exp=21600):
+        print cherrypy.request.config
+        return self.mem.set(key, value, exp)
+    set = dumps
+    
